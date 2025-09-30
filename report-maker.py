cd118a0091af6c295b58756cf4e84b81e7f25ed5
potte40 <potte40@hotmail.com>
2025-09-30 15:40:08 +0200

    Created a txt called report

--- report-maker.py
+++ report-maker.py
@@ -1,18 +1,23 @@
 # Import the json library so that we can handle json
-
 import json
 
 # Read json from products.json to the variable products
-
 # open(filePath, "r" for read, encoding = character encoding)
 
 data = json.load(open("network_devices.json","r",encoding = "utf-8"))
 
+# Create a variable that holds our whole text report
+report = ""
+
 # loop through the location list
 for location in data["locations"]:
-    # print the site/"name" of the location
-    print("")
-    print(location["site"])
-    # print a list of the host names of the devices on the location
+    # add the site/"name" of the location to the report
+    report += "\n" + location["site"] + "\n"
+    # add a list of the host names of the devices
+    # on the location to the report
     for device in location["devices"]:
-        print(" ",device["hostname"])
+        report += "  " + device["hostname"] + "\n"
+
+# write the report to text file
+with open('report.txt', 'w', encoding='utf-8') as f:
+    f.write(report)
