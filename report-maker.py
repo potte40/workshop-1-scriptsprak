abd46b526393d6128d94631a7e641489773c03e5
potte40 <potte40@hotmail.com>
2025-10-02 10:36:12 +0200

    Ändrat till snyggare layout i rapporten

--- report-maker.py
+++ report-maker.py
@@ -16,7 +16,21 @@ for location in data["locations"]:
     # add a list of the host names of the devices
     # on the location to the report
     for device in location["devices"]:
-        report += "  " + device["hostname"] + "\n"
+        report += ("  " 
+                   + device["hostname"].ljust(15) + " "
+                   + device["vendor"].ljust(15) + " "
+                   + str(device["uptime_days"]).rjust(4) + " " 
+                   + "\n")
+
+# Create an empty summary
+summary = ""
+
+# Somewhere later in our report add something to summary
+summary += "Summary:\n"
+summary += "This is our basic report:"
+
+# Add summary before main report
+report = summary + report
 
 # write the report to text file
 with open('network_report.txt', 'w', encoding='utf-8') as f:
